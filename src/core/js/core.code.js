39962e97ac32f06ba20a54b79481efd2718f76dc
John <Johnnathansn@gmail.com>
2016-01-26 11:22:05 -0800

    update

--- src/core/js/core.code.js
+++ src/core/js/core.code.js
@@ -18,23 +18,29 @@
             date   : ""
         };
 
-        vm.msgView = false;
+        vm.showList = false;
+        vm.msgView  = false;
         vm.messages = [];
 
         // function list
-        vm.sendMsg    = sendMsg;
-        vm.previewMsg = previewMsg;
-        vm.add        = add;
+        vm.sendMsg     = sendMsg;
+        vm.previewMsg  = previewMsg;
+        vm.add         = add;
+        vm.showMsgList = showMsgList;
 
 
 
 
         // functions code
+
+        // remake send to interact with the backend
+        // will save the mesage list to DB
         function sendMsg() {
             vm.add(vm.newMessage);
             return true;
         }
 
+        // show message preview
         function previewMsg() {
             if (vm.msgView == false)
                 vm.msgView = true;
@@ -42,7 +48,19 @@
                 vm.msgView = false;
         }
 
+        // show messages based on count
+        function showMsgList(){
+            if( vm.messages.length > 0 )
+                vm.showList = true;
+            else
+                vm.showList = false;
+        }
+
+        // adds message to the list
         function add(newMsg){
+            if( newMsg.subject == "" || newMsg.body == "" )
+                    return false;
+
             newMsg.date = (vm.date.getFullYear() + '-' + ('0' + (vm.date.getMonth() + 1)).slice(-2) + '-' + ('0' + vm.date.getDate()).slice(-2))
             vm.messages.push(newMsg);
             vm.newMessage = {
@@ -50,9 +68,11 @@
                 body   : "",
                 date   : ""
             };
+            vm.showMsgList();
             return true;
         }
 
+        // work in progress
         function listMsgs() {
 
         }
